d9da7cca889e68456bdc22c4d79e1f08a7e8fadc
benvanwerkhoven <ben@cs.vu.nl>
2012-11-02 10:56:59 +0100

    made TRACER a pointer in addition to an allocatable, not sure if this works....

--- source/prognostic.F90
+++ source/prognostic.F90
@@ -40,7 +40,7 @@
 !      target :: &
 !      TRACER     ! 3d tracer fields for all blocks at 3 time levels
 !
-   real (r8), dimension(:,:,:,:,:,:), allocatable :: &
+   real (r8), dimension(:,:,:,:,:,:), allocatable, pointer :: &
       TRACER     ! 3d tracer fields for all blocks at 3 time levels
 
    type (tracer_field), dimension(nt) :: &
