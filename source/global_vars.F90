a11da63b2305454af799d1fde123ff1bd634fa71
benvanwerkhoven <ben@cs.vu.nl>
2013-03-21 10:06:13 +0100

    test

--- source/global_vars.F90
+++ source/global_vars.F90
@@ -29,11 +29,11 @@
 
 ! !PUBLIC DATA MEMBERS:
 
-   real (r8), dimension(:,:,:,:,:), public, pointer :: &
+   real (r8), dimension(:,:,:,:,:), pointer :: &
       VDC, &             ! tracer diffusivity - public to allow possible modification by Gent-McWilliams horizontal mixing parameterization
       VDCREF             ! copy for GPU result verification
 
-   real (r8), dimension(:,:,:,:), public, pointer :: &
+   real (r8), dimension(:,:,:,:), pointer :: &
       VVC,         &! momentum viscosity
       VVCREF        ! copy for GPU result verification
 
