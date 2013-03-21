eaa486e89317afbc711c3ae7a0173f24ac3e244e
benvanwerkhoven <ben@cs.vu.nl>
2013-03-21 11:54:48 +0100

    typo

--- source/gpu_cmod.cu
+++ source/gpu_cmod.cu
@@ -725,19 +725,23 @@ __global__ void ddmix_kernelmm(double *VDC1, double *VDC2, double *TEMP, double 
   if (i < NX_BLOCK*NY_BLOCK*(end_k-start_k)) {
 
 #ifdef USE_READ_ONLY_CACHE
-  if (k < KM-1) {
+
    temp_kup = __ldg(TEMP+index);
    salt_kup = __ldg(SALT+index);
-  }
+
+  if (k < KM-1) {
    temp = __ldg(TEMP+indexpk);
    salt = __ldg(SALT+indexpk);
+  }
 #else
-  if (k < KM-1) {
+ 
    temp_kup = TEMP[index];
    salt_kup = SALT[index];
-  }
+
+  if (k < KM-1) {
    temp = TEMP[indexpk];
    salt = SALT[indexpk];
+  }
 #endif
 
    //inlined function state
